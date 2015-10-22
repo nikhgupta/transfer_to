5335fcd11a054552e0f2fad1fb344456a3a0adb4
Luis Doubrava <luis@cg.nl>
2015-10-22 09:11:51 +0200

    add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require 'rubygems'
+require 'bundler'
+
+# require these if you need to test with pry
+require 'byebug'
+require 'pry-byebug'
+
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts 'Run `bundle install` to install missing gems'
+  exit e.status_code
+end
+
+require 'transfer_to'
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+end
