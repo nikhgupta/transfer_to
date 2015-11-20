ef1469d550dca5014c1c87b13e3d053fac9812ad
gjselten <geert-jan@cg.nl>
2015-11-20 11:25:14 +0100

    created trans_info, trans_list and get_id_from_key

---
+++ lib/transfer_to/trans_list.rb
@@ -0,0 +1,57 @@
+module TransferToApi
+  class TransList < Base
+
+    attr_reader :transaction_ids
+
+    # This method is used to retrieve the list of transactions performed within
+    # the date range by the MSISDN if set. Note that both dates are included
+    # during the search.
+    #
+    # parameters
+    # ==========
+    # msisdn
+    # ------
+    # The format must be international with or without the ‘+’ or ‘00’:
+    # “6012345678” or “+6012345678” or “006012345678” (Malaysia)
+    #
+    # destination_msisdn
+    # ------------------
+    # The format must be international with or without the ‘+’ or ‘00’:
+    # “6012345678” or “+6012345678” or “006012345678” (Malaysia)
+    #
+    # code
+    # ----
+    # The error_code of the transactions to search for. E.g “0” to search for
+    # only all successful transactions. If left empty, all transactions will be
+    # returned(Failed and successful).
+    #
+    # start_date
+    # ----------
+    # Defines the start date of the search. Format must be YYYY-MM-DD.
+    #
+    # stop_date
+    # ---------
+    # Defines the end date of the search (included). Format must be YYYY-MM-DD.
+    def self.get(start_date, end_date, msisdn=nil, destination=nil, code=nil)
+      params = { }
+
+      params[:code] = code unless code
+      params[:msisdn] = msisdn unless msisdn
+      params[:stop_date] = end_date
+      params[:start_date] = start_date
+      params[:destination_msisdn] = destination unless destination
+
+      response = run_action :trans_list, params
+      TransferToApi::TransList.new(response)
+    end
+
+
+    def initialize(response)
+      super(response)
+      @transaction_ids = response.data[:transaction_list].split(',')
+    end
+  end
+end
+
+# list = TransferToApi::TransList.login('rechargeops', 'uGyRrKfyTP').get('2015-11-01', '2015-11-20')
+
