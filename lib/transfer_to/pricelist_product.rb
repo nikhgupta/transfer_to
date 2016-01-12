61e3a8240930fa5ba51089f4910f6c465cbdee58
gjselten <geert-jan@cg.nl>
2016-01-12 13:47:25 +0100

    added skuid to pricelist_product

--- lib/transfer_to/pricelist_product.rb
+++ lib/transfer_to/pricelist_product.rb
@@ -1,10 +1,11 @@
 module TransferToApi
   class Product
-    attr_reader :operator, :destination_currency
+    attr_reader :operator, :destination_currency, :skuid
 
     def initialize(response)
       @operator = TransferToApi::Operator.new(response.data[:country], response.data[:countryid], response.data[:operator], response.data[:operatorid])
       @destination_currency = response.data[:destination_currency]
+      @skuid = response.data[:skuid]
     end
   end
 end
