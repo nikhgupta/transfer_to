5675426681b06aa054cae35018477e44e9b83be0
gjselten <geert-jan@cg.nl>
2016-03-21 09:23:12 +0100

    added product type to both open_range and fixed_denom product; bump version

--- lib/transfer_to/pricelist_open_range_product.rb
+++ lib/transfer_to/pricelist_open_range_product.rb
@@ -3,7 +3,7 @@ module TransferToApi
     attr_reader :minimum_amount_local_currency,
     :maximum_amount_local_currency, :minimum_amount_requested_currency,
     :maximum_amount_requested_currency,:increment_local_currency,
-    :requested_currency, :open_range_wholesale_discount, :product_type,
+    :requested_currency, :open_range_wholesale_discount,
     :open_range_wholesale_rate
 
     def initialize(response)
@@ -13,7 +13,6 @@ module TransferToApi
       @maximum_amount_requested_currency = response.data[:open_range_maximum_amount_requested_currency]
       @increment_local_currency = response.data[:open_range_increment_local_currency]
       @requested_currency = response.data[:open_range_requested_currency]
-      @product_type = response.data[:product_type]
       @open_range_wholesale_rate = response.data[:open_range_wholesale_rate]
       @open_range_wholesale_discount = response.data[:open_range_wholesale_discount]
       super(response)
--- lib/transfer_to/pricelist_product.rb
+++ lib/transfer_to/pricelist_product.rb
@@ -1,6 +1,7 @@
 module TransferToApi
   class Product
-    attr_reader :operator, :destination_currency, :skuid, :open_range
+    attr_reader :operator, :destination_currency, :skuid, :open_range,
+    :product_type
 
     def initialize(response)
       @open_range = response.data[:open_range] || "0"
@@ -10,6 +11,7 @@ module TransferToApi
                                               response.data[:operatorid])
       @destination_currency = response.data[:destination_currency]
       @skuid = response.data[:skuid]
+      @product_type = response.data[:product_type]
     end
 
     def is_pin_based?
